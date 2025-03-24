71231d5e6fa50e2671eb064ec52806bb4f0d9fc0
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-03-23 22:17:46 -0600

    Agregar archivos de proyecto.

---
+++ PlatyPlus/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PlatyPlus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
